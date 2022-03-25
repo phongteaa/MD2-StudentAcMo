8d3a9353e6f58f2e112bb5c98c7b99e77f239417
phongteaa <phongteaa@gmail.com>
2022-03-25 10:52:02 +0700

    1

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        Student st1 = new Student();
+        System.out.println(st1);
+
+        st1.setName("HuanRose");
+        st1.setClasses("XXX");
+        System.out.println(st1);
+    }
+}
